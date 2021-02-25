6eca22ad3d919bd6fbe1c95eb172ae9396a1068e
Connie <leungcnie@gmail.com>
2021-02-25 15:34:38 -0800

    Parse API JSON and display in erb

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,4 +1,9 @@
+require 'net/http'
+require 'json'
+
 class UpdatesController < ApplicationController
   def index
+    uri = URI('https://raw.githubusercontent.com/Vericatch/devtestapiapp/master/mock_response.json')
+    @updates = JSON.parse(Net::HTTP.get(uri))
   end
 end
